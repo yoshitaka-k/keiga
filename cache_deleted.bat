1ac93761edf8e5ec09386fe7b0d4e0f0466f6135
Yoshitaka <yoshitaka@ktgwya.jp>
2026-08-25 13:39:58 +0900

    chore(sh): app cache deleted script

---
+++ cache_deleted.bat
@@ -0,0 +1,3 @@
+@echo off
+
+if exist "%APPDATA%\Keiga" rd /s /q "%APPDATA%\Keiga" 2>nul
--- cache_deleted.sh
+++ cache_deleted.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-rm -rf ~/Library/Application\ Support/Img-Optim-*
-rm -rf ~/Library/Application\ Support/Img-Optim
-rm -rf ~/Library/Application\ Support/Keiga-*
-rm -rf ~/Library/Application\ Support/Keiga
+case "$(uname -s)" in
+Darwin)
+  rm -rf ~/Library/Application\ Support/Keiga
+  ;;
+Linux)
+  data_home="${XDG_DATA_HOME:-$HOME/.local/share}"
+  rm -rf "$data_home"/keiga
+  ;;
+MINGW*|MSYS*|CYGWIN*)
+  rm -rf "$APPDATA"/Keiga
+  ;;
+*)
+  echo "unsupported OS: $(uname -s)" >&2
+  exit 1
+  ;;
+esac
 
 exit
